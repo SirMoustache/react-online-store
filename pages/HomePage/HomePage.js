96cbebdd09b5cb3f6ad1bbbafff36361dc018489
Vitaliy Hopanyuk <sethsleerk@gmail.com>
2019-01-10 21:06:49 +0200

    Added HomePage placeholder

---
+++ pages/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+/**
+ * Absolute imports
+ */
+import React, { Fragment } from 'react';
+
+const HomePage = () => (
+  <Fragment>
+    <h1>Hello World from Home Page</h1>
+  </Fragment>
+);
+
+export default HomePage;
